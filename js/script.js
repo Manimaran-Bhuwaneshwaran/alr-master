ffed42d83387af8c82a7e827a2ce9dc44368a648
Manimaran-Bhuwaneshwaran <62472418+Manimaran-Bhuwaneshwaran@users.noreply.github.com>
2020-07-23 19:34:30 +0530

    header,home,testimonial,footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+function showNav()
+{
+    document.getElementById("content").classList.toggle("show-nav");
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("content").style.display="block";
+    document.body.classList.toggle("overflow")
+}
