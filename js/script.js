c1064a8e296fa875cbe3bbbb6fff4fe5ae8fa353
Manimaran-Bhuwaneshwaran <62472418+Manimaran-Bhuwaneshwaran@users.noreply.github.com>
2020-07-24 14:10:56 +0530

    testimonial,header

--- js/script.js
+++ js/script.js
@@ -2,6 +2,5 @@ function showNav()
 {
     document.getElementById("content").classList.toggle("show-nav");
     document.getElementById("menu").classList.toggle("change");
-    document.getElementById("content").style.display="block";
     document.body.classList.toggle("overflow")
 }
