023cb0951e3822a9fa79e9fa74e86ba73efb04b3
Manimaran-Bhuwaneshwaran <62472418+Manimaran-Bhuwaneshwaran@users.noreply.github.com>
2020-07-25 20:31:21 +0530

    prcing

--- js/script.js
+++ js/script.js
@@ -2,6 +2,30 @@ function showNav()
 {
     document.getElementById("content").classList.toggle("show-nav");
     document.getElementById("menu").classList.toggle("change");
-    document.body.classList.toggle("overflow")
 }
 
+function showModal()
+{
+    document.getElementById("modal").style.display="block";
+    document.body.style.overflow="hidden";
+}
+function closeModal()
+{
+    document.getElementById("modal").style.display="none";
+    document.body.style.overflow="auto";
+}
+function mvalidate()
+{
+    var numbers = /^[0-9]+$/;
+    var contact=document.getElementById("contactNumber");
+    if(contact.value.match(numbers))
+    {
+        return true;
+    }
+    else
+    {
+        alert('Please enter valid number');
+        contact.focus();
+        return false;
+    }
+} 
